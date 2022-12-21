a78c7c88403e6d15c9a6d958cbf2384bf419ff56
SuperCrabLover <molozhavenko.aa@phystech.edu>
2022-12-21 14:05:27 +0300

    added more flexibility to arch

--- pgm.py
+++ pgm.py
@@ -2,29 +2,74 @@ import numpy as np
 import math
 import tensornetwork as tn
 from albumentations.augmentations.crops.transforms import CenterCrop
-import logging
 
-def create_node(matrix, tensor_base):
+def create_tensor(matrix, tensor_base):
+  '''
+  INPUT:
+  matrix     -- 2d ndarray -- image to tensorize
+  tensor_base --    int     -- the dimension of each mode for a tensor
   
-  if len(matrix.shape) > 2:
-    logging.error(f"create_node error: len(matrix.shape) ({len(matrix.shape)}) is more than {2}")
-    return None, None
+  OUTPUT:
+  tensor     -- Nd ndarray -- an image in tensor represenation
+  N          --    int     -- amount of modes in output tensor (N = log_{tensor_base}(matrix.shape[0] * matrix.shape[1]))
+  '''
   
   pix_am = matrix.shape[0] * matrix.shape[1]
   N = math.log(pix_am, tensor_base)
   if not N.is_integer():
-    logging.error(f"create_node error: the tensor rank {N} is not an integer")
-    return None, None
+    raise Exception(f"create_node error: the tensor rank {N} is not an integer")
+  
+  N = int(N)
+  return np.reshape(matrix, tuple([tensor_base] * N)), N
 
-  return tn.Node(np.reshape(matrix, tuple([tensor_base] * int(N)))), int(N) 
+def create_node(matrix, tensor_base):
+  '''
+  INPUT:
+  matrix     -- 2d ndarray -- image to tensorize
+  tensor_base --    int     -- the dimension of each mode for a tensor
+  
+  OUTPUT:
+  Node       -- Tensor Network Node -- an image in TN Node represenation
+  N          --         int         -- amount of modes in output tensor (N = log_{tensor_base}(matrix.shape[0] * matrix.shape[1]))
+  '''
+  
+  if len(matrix.shape) > 2:
+    raise Exception(f"create_node error: len(matrix.shape) ({len(matrix.shape)}) is more than {2}")
+  
+  tensor, N = create_tensor(matrix, tensor_base)
+  
+  return tn.Node(tensor), N
+  
+  
 
 def crop_pgm_image(im_pgm, width=512, height=512):
+  '''
+  INPUT:
+  im_pgm -- 2d ndarray -- input image to crop
+  width  --     int    -- the width of output image
+  height --     int    -- the height of output image
+  
+  OUTPUT:
+  im_pgm -- 2d ndarray -- cropped image 
+  '''
+  
   resizer = CenterCrop(width=width, height=height)
   im_pgm = resizer(image=im_pgm)["image"]
   
   return im_pgm 
 
 def create_node_from_pgm(im_pgm, tensor_base, width=512, height=512):
+  '''
+  INPUT:
+  im_pgm -- 2d ndarray -- input image to crop
+  width  --     int    -- the width of output image
+  height --     int    -- the height of output image
+  tensor_base --    int     -- the dimension of each mode for a tensor
+  
+  OUTPUT:
+  Node       -- Tensor Network Node -- an image in TN Node represenation
+  N          --         int         -- amount of modes in output tensor (N = log_{tensor_base}(matrix.shape[0] * matrix.shape[1]))
+  '''
   
   if im_pgm.shape[1] != width or im_pgm.shape[0] != height: 
     im_pgm = crop_pgm_image(im_pgm, width=width, height=height)
